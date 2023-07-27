e784ff23f3c51438a54976ea01f7d11c46c04d29
root <fentseramushu@gmail.com>
2023-07-27 13:39:49 -0700

    files

--- utils.c
+++ utils.c
@@ -1,18 +1,21 @@
 #include "main.h"
 
+
 /**
- * is_printable - Evaluates if a char is printable.
+ * is_printable - Evaluates if a char is printable
  * @c: Char to be evaluated.
  *
  * Return: 1 if c is printable, 0 otherwise
  */
 int is_printable(char c)
 {
-    if (c >= 32 && c < 127)
-        return (1);
+        if (c >= 32 && c < 127)
+                return (1);
 
-    return (0);
+
+        return (0);
 }
+
 
 /**
  * append_hexa_code - Append ascci in hexadecimal code to buffer
@@ -23,19 +26,23 @@ int is_printable(char c)
  */
 int append_hexa_code(char ascii_code, char buffer[], int i)
 {
-    char map_to[] = "0123456789ABCDEF";
-    /* The hexa format code is always 2 digits long */
-    if (ascii_code < 0)
-        ascii_code *= -1;
+        char map_to[] = "0123456789ABCDEF";
+        /* The hexa format code is always 2 digits long */
+        if (ascii_code < 0)
+                ascii_code *= -1;
 
-    buffer[i++] = '\\';
-    buffer[i++] = 'x';
 
-    buffer[i++] = map_to[ascii_code / 16];
-    buffer[i] = map_to[ascii_code % 16];
+        buffer[i++] = '\\';
+        buffer[i++] = 'x';
 
-    return (3);
+
+        buffer[i++] = map_to[ascii_code / 16];
+        buffer[i] = map_to[ascii_code % 16];
+
+
+        return (3);
 }
+
 
 /**
  * is_digit - Verifies if a char is a digit
@@ -45,11 +52,13 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
  */
 int is_digit(char c)
 {
-    if (c >= '0' && c <= '9')
-        return (1);
+        if (c >= '0' && c <= '9')
+                return (1);
 
-    return (0);
+
+        return (0);
 }
+
 
 /**
  * convert_size_number - Casts a number to the specified size
@@ -60,13 +69,15 @@ int is_digit(char c)
  */
 long int convert_size_number(long int num, int size)
 {
-    if (size == S_LONG)
-        return (num);
-    else if (size == S_SHORT)
-        return ((short)num);
+        if (size == S_LONG)
+                return (num);
+        else if (size == S_SHORT)
+                return ((short)num);
 
-    return ((int)num);
+
+        return ((int)num);
 }
+
 
 /**
  * convert_size_unsgnd - Casts a number to the specified size
@@ -77,10 +88,11 @@ long int convert_size_number(long int num, int size)
  */
 long int convert_size_unsgnd(unsigned long int num, int size)
 {
-    if (size == S_LONG)
-        return (num);
-    else if (size == S_SHORT)
-        return ((unsigned short)num);
+        if (size == S_LONG)
+                return (num);
+        else if (size == S_SHORT)
+                return ((unsigned short)num);
 
-    return ((unsigned int)num);
+
+        return ((unsigned int)num);
 }
